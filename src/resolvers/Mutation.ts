3db8b1bb9051ce1cc9f0531fa24a268a3f3a67a0
Martin Tupy <martintupyy@gmail.com>
2020-08-04 00:44:39 +0200

    Implemented Sync coins

--- src/resolvers/Mutation.ts
+++ src/resolvers/Mutation.ts
@@ -3,25 +3,37 @@ import { Coin, MutationAddCoinArgs } from '../types'
 import fetch from 'node-fetch'
 
 export const Mutation = {
-  async addCoin(parent: any, args: MutationAddCoinArgs, ctx: Context): Promise<Coin> {
-    const coin = await ctx.prisma.coin.create({
-      data: {
-        coin_id: args.coinId,
-        name: args.name,
-        symbol: args.symbol
+  async addCoin(parent: any, args: MutationAddCoinArgs, ctx: Context): Promise<Coin | null> {
+    const foundCoin = await ctx.prisma.coin.findOne({
+      where: {
+        coin_id: args.coinId
       }
     })
-    return CoinIso.get(coin)
+    if (!!foundCoin) {
+      return null
+    } else {
+      const coin = await ctx.prisma.coin.create({
+        data: {
+          coin_id: args.coinId,
+          name: args.name,
+          symbol: args.symbol
+        }
+      })
+      return CoinIso.get(coin)
+    }
   },
 
-  async syncCoins(parent: any, args: any, ctx: Context): Promise<Coin[]> {
-    return fetch('https://api.coingecko.com/api/v3/coins/list').then(async response => {
-      const apiCoins: ApiCoin[] = await response.json()
-      return Promise.all(apiCoins.map(apiCoin => ({
+  async syncCoins(parent: any, args: any, ctx: Context) {
+    const res = await fetch('https://api.coingecko.com/api/v3/coins/list')
+    const apiCoins: ApiCoin[] = await res.json()
+    const coins = await Promise.all(apiCoins.map(apiCoin => {
+      const args = {
         coinId: apiCoin.id,
         name: apiCoin.name,
         symbol: apiCoin.symbol
-      } as MutationAddCoinArgs)).map(coin => Mutation.addCoin(parent, coin, ctx)))
-    })
+      } as MutationAddCoinArgs
+      return Mutation.addCoin(parent, args, ctx)
+    }))
+    return coins.filter(coin => !!coin) //as Coin[]
   }
 }
